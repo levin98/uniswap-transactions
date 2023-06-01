a82e9d067defc0cb2fcd16722782141d74eca761
Yu Zhe <kelvinlim_98@live.com>
2023-06-01 15:13:54 +0800

    Add database connection and config file

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import Config
+from app.extensions import db
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    # Initialize database
+    db.init_app(app)
 
     return app
