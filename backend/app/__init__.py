9cc9eb5609d513140c0aa583f45ddc0625358abf
Yu Zhe <kelvinlim_98@live.com>
2023-06-01 17:28:33 +0800

    Add get transaction by hash endpoint

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from app.models.transaction import Transaction
 from app.extensions import db
 from app.scheduler import scheduler
+from app.etherscan import api
 import functools
 
 
@@ -44,6 +45,31 @@ def create_app(config_class=Config):
                 "pages": transactions.pages,
             },
         }
-        return jsonify(results)
+        return jsonify(results), 200
+
+    @app.route("/transaction", methods=['GET'])
+    def get_transaction_by_hash():
+        """Get transaction by hash from database"""
+
+        # Get query parameters
+        hash = request.args.get("hash", None, type=str)
+        if hash is None:
+            return jsonify({"error": "No hash provided"}), 400
+
+        transaction = Transaction.query.filter_by(txn_hash=hash).first()
+        if transaction is None:
+            transaction = api.get_transaction_by_hash(hash)
+            if transaction is None:
+                return jsonify({"error": "Transaction not found"}), 404
+
+            results = {
+                "block_number": int(transaction['blockNumber'], base=16),
+                "txn_hash": transaction['transactionHash'],
+                "gas_price": str(int(transaction['effectiveGasPrice'], base=16)),
+                "gas_used": str(int(transaction['gasUsed'], base=16)),
+            }
+            return jsonify(results), 200
+
+        return jsonify(transaction.to_dict()), 200
 
     return app
