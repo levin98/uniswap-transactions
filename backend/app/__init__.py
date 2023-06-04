a3e16140b269ba9a0d31d7ffee86af07f5381a32
Yu Zhe <kelvinlim_98@live.com>
2023-06-04 21:52:11 +0800

    Add CORS to backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,17 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from config import Config
 from app.models.transaction import Transaction
 from app.extensions import db
 from app.scheduler import scheduler
 from app.etherscan import api
+from datetime import datetime
 import functools
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config_class)
 
     # Initialize database
@@ -25,15 +28,20 @@ def create_app(config_class=Config):
 
         # Get query parameters
         page = request.args.get("page", 1, type=int)
-        per_page = request.args.get("per-page", 50, type=int)
-        start_time = request.args.get("start-time", None, type=int)
-        end_time = request.args.get("end-time", None, type=int)
+        per_page = request.args.get("pageSize", 50, type=int)
+        start_time = request.args.get("from", None, type=int)
+        end_time = request.args.get("to", None, type=int)
 
         if (start_time is not None) and (end_time is not None):
+            start_time = datetime.fromtimestamp(int(start_time)).replace(
+                hour=0, minute=0, second=0, microsecond=0)
+            end_time = datetime.fromtimestamp(int(end_time)).replace(
+                hour=0, minute=0, second=0, microsecond=0)
+
             transactions = Transaction.query.filter(
-                Transaction.txn_timestamp.between(start_time, end_time)).paginate(page=page, per_page=per_page)
+                Transaction.txn_timestamp.between(start_time, end_time)).order_by(Transaction.txn_timestamp.desc()).paginate(page=page, per_page=per_page)
         else:
-            transactions = Transaction.query.paginate(
+            transactions = Transaction.query.order_by(Transaction.txn_timestamp.desc()).paginate(
                 page=page, per_page=per_page)
 
         results = {
@@ -59,7 +67,7 @@ def create_app(config_class=Config):
         transaction = Transaction.query.filter_by(txn_hash=hash).first()
         if transaction is None:
             transaction = api.get_transaction_by_hash(hash)
-            if transaction is None:
+            if transaction is None or len(transaction) == 0:
                 return jsonify({"error": "Transaction not found"}), 404
 
             results = {
