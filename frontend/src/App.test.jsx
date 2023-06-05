1cb0dc24105892bbb4f3ef42a551d47e64ee9f92
Yu Zhe <kelvinlim_98@live.com>
2023-06-05 16:11:43 +0800

    Integrate TransactionList to App component

--- frontend/src/App.test.jsx
+++ frontend/src/App.test.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import "./mock"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// eslint-disable-next-line react/display-name
+jest.mock('./components/TransactionList', () => () => <div data-testid="transaction-list">Mocked Transaction List</div>);
+
+test('renders App with Transaction List', () => {
+  const { container } = render(<App />);
+  expect(screen.getByTestId("transaction-list")).toBeInTheDocument();
+  expect(container).toMatchSnapshot();
 });
